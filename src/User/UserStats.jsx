4113b6b1b99d9f9c9fcf14faf6834392cebcbc5f
Paulo Roberto Abrahão de Souza <ab.cod@outlook.com>
2022-02-01 15:56:58 -0300

    Ajustes finais e finalizacao do projeto

--- src/User/UserStats.jsx
+++ src/User/UserStats.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
-import Head from '../Components/Helper/Head';
+import React from "react";
+import { STATS_GET } from "../api";
+import Head from "../Components/Helper/Head";
+import useFetch from "../Hooks/useFetch";
+import Loading from "../Components/Helper/Loading";
+import Error from "../Components/Helper/Error";
+const UserStatsGraphs = React.lazy(() => import('./UserStatsGraphs'))
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title='Estatísticas' />
-      estatisticas
-    </div>
-  );
+  const { data, loading, error, request } = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET()
+      await request(url, options)
+    }
+    getData()
+  }, [request]);
+
+  if (loading) return <Loading />
+  if (error) return <Error error={error} />
+  if (data)
+    return (
+      <React.Suspense fallback={<div></div>}>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} /> 
+      </React.Suspense>
+    );
+  else return null
 };
 
 export default UserStats;
